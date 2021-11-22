086229e7a8cedba98c0a986cd3d09f61e159bd62
KireUusimies <46391855+KireUusimies@users.noreply.github.com>
2021-11-22 01:11:51 -0500

    Create cool_timeline.cpp

---
+++ cool_timeline.cpp
@@ -0,0 +1,303 @@
+<html>
+    <head>
+        <title>Cool Timeline</title>
+        <meta charset="utf-8">
+        <style>
+            body {
+                background-color: rgb(0, 160, 168);
+            }
+            #header {
+                color: rgb(255, 255, 255);
+                background: rgb(0, 0, 168);
+                height: 90px;
+                padding:8px;
+                border: 3px solid rgb(0, 0, 168);
+            }
+            #menu {
+                background-color: rgb(255, 255, 255);
+                height: 1,000px;
+                padding: 8px;
+                border: 3px solid rgb(0, 0, 168);
+                border-top: 0px;
+            }
+            #footer {
+                background-color: rgb(0, 0, 168);
+                color: rgb(255, 255, 255);
+                text-align: center;
+                height: 51px;
+                padding:8px;
+                border: 3px solid rgb(0, 0, 168);
+                border-top: 3px;
+            }
+            #container {
+                width:700px;
+                position: absolute;
+                font-size: 20px;
+                position: absolute;
+                left: 325px;
+                width: 890px;
+                font-family: sans-serif;
+            }
+            .three {
+                text-align: left;
+            }
+            a:link {
+                color: rgb(0, 0, 0);
+                text-decoration: none;
+            }
+            a:visited {
+                color: rgb(0, 0, 0);
+            }
+            a:hover {
+                -webkit-animation: link-blink 1s infinite;
+                -moz-animation: link-blink 1s infinite;
+                animation: link-blink 1s infinite;
+            }
+            @-webkit-keyframes link-blink {
+                 0% { background-color: rgb(0, 0, 255); }
+                 0% { color: rgb(255, 255, 255); }
+                 49% { background-color: rgb(0, 0, 255); }
+                 49% { color: rgb(255, 255, 255); }
+                 50% { background-color: rgb(255, 255, 255); }
+                 50% { color: rgb(0, 0, 0); }
+                 99% { background-color: rgb(255, 255, 255); }
+                 99% { color: rgb(0, 0, 0); }
+                 100% { background-color: rgb(0, 0, 255); }
+                 100% { color: rgb(255, 255, 255); }
+            }
+             @-moz-keyframes link-blink {
+                 0% { background-color: rgb(0, 0, 255); }
+                 0% { color: rgb(255, 255, 255); }
+                 49% { background-color: rgb(0, 0, 255); }
+                 49% { color: rgb(255, 255, 255); }
+                 50% { background-color: rgb(255, 255, 255); }
+                 50% { color: rgb(0, 0, 0); }
+                 99% { background-color: rgb(255, 255, 255); }
+                 99% { color: rgb(0, 0, 0); }
+                 100% { background-color: rgb(0, 0, 255); }
+                 100% { color: rgb(255, 255, 255); }
+            }
+            @keyframes link-blink {
+                 0% { background-color: rgb(0, 0, 255); }
+                 0% { color: rgb(255, 255, 255); }
+                 49% { background-color: rgb(0, 0, 255); }
+                 49% { color: rgb(255, 255, 255); }
+                 50% { background-color: rgb(255, 255, 255); }
+                 50% { color: rgb(0, 0, 0); }
+                 99% { background-color: rgb(255, 255, 255); }
+                 99% { color: rgb(0, 0, 0); }
+                 100% { background-color: rgb(0, 0, 255); }
+                 100% { color: rgb(255, 255, 255); }
+            }
+        </style>
+</head>
+<body>
+  <div id="container">
+    <div id="header">
+      <p><strong>Cool Timeline</strong></p>
+    </div>
+    <div id="menu">
+      <ul>
+        <li>2021</li>
+          <ul>
+            <li>Podcasts</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UC5tigjL4SYA_nWP-E8-eiBw">Trash Tuesday</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/c/BadFriends/featured">Bad Friends</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UCIyIoM_Nd8HtY19fuR_ov2A">TigerBelly</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UC8EKPRFP1NaaWIR06NtjqkQ">The Steebee Weebee Show</a></li>
+              </ul>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=bth7ViGGpeY">Internet Money - His & Hers</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Cobra_Kai">Cobra Kai</a></li>
+          </ul>
+        <li>2020</li>
+          <ul>
+            <li>Podcasts</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/c/BadFriends/featured">Bad Friends</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UCIyIoM_Nd8HtY19fuR_ov2A">TigerBelly</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UC8EKPRFP1NaaWIR06NtjqkQ">The Steebee Weebee Show</a></li>
+              </ul>
+            <li>Music</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/watch?v=5PMcZ_z-M5Q">Jack Champ - The Tigerbelly Song</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/watch?v=A7rKlwzuFgY">Good Terms - Light At the End</a></li>
+              </ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+          </ul>
+        <li>2019</li>
+          <ul>
+            <li>Podcasts</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UCIyIoM_Nd8HtY19fuR_ov2A">TigerBelly</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UC8EKPRFP1NaaWIR06NtjqkQ">The Steebee Weebee Show</a></li>
+              </ul>
+            <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_mao9YHfZtBBTlxeT138lO0prcGHzNSWQM">Tyler, The Creator - Igor</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Cobra_Kai">Cobra Kai</a></li>
+          </ul>
+        <li>2018</li>
+          <ul>
+            <li>Podcasts</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UCIyIoM_Nd8HtY19fuR_ov2A">TigerBelly</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UC8EKPRFP1NaaWIR06NtjqkQ">The Steebee Weebee Show</a></li>
+              </ul>
+            <li>Entertainment</li>
+              <ul>
+                <li><a target="_blank" href="https://en.wikipedia.org/wiki/Cobra_Kai">Cobra Kai</a></li>
+                <li><a target="_blank" href="https://en.wikipedia.org/wiki/Flavors_of_Youth">Flavors of Youth</a></li>
+              </ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+          </ul>
+        <li>2017</li>
+          <ul>
+            <li>Podcasts</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UCIyIoM_Nd8HtY19fuR_ov2A">TigerBelly</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UC8EKPRFP1NaaWIR06NtjqkQ">The Steebee Weebee Show</a></li>
+              </ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=xe2FPP4lX14">Absofacto - Dissolve</a></li>
+          </ul>
+        <li>2016</li>
+          <ul>
+            <li>Podcasts</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/channel/UCIyIoM_Nd8HtY19fuR_ov2A">TigerBelly</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+              </ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+          </ul>
+        <li>2015 - <a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+        <li>2014</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=mgbMqbvlEJA">Send Request - Comeback Song</a></li>
+          </ul>
+        <li>2013</li>
+          <ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Too_Weird_to_Live,_Too_Rare_to_Die!">Panic! At The Disco - Too Weird to Live, Too Rare to Die!</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/c/joerogan">The Joe Rogan Experience</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+          </ul>
+        <li>2012 - <a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+        <li>2011</li>
+          <ul>
+            <li>Video Games</li>
+              <ul>
+                <li><a target="_blank" href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a></li>
+                <li><a target="_blank" href="https://en.wikipedia.org/wiki/Fallout:_New_Vegas">Fallout: New Vegas</a></li>
+              </ul>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=JB9ArM4xUjg">Panic! At The Disco - Sarah Smiles</a></li>
+          </ul>
+        <li>2010</li>
+          <ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Fallout:_New_Vegas">Fallout: New Vegas</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_mQW4TjT0Hml9rr-LYk8Nin0kDajNspYgQ">Tame Impala - Innerspeaker</a></li>
+          </ul>
+        <li>2009</li>
+          <ul>
+            <li>Music</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_mveVvF9X_kSFtIBDgbtC46CyXHTvN2iBM">Paramore - Brand New Eyes</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/watch?v=pWqueQ8JO9E">Tame Impala - Sundown Syndrome</a></li>
+              </ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/SpongeBob%27s_Truth_or_Square_(video_game)">SpongeBob's Truth or Square</a></li>
+          </ul>
+        <li>2008</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/playlist?list=PL59C728E5D27BB14B">The Messenger - Let's Get Dangerous!</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=jFg_8u87zT0">The Fray - You Found Me</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Tame_Impala_(EP)">Tame Impala - Tame Impala</a></li>
+          </ul>
+        <li>2007</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=qi7Yh16dA0w">Sara Bareilles - Love Song</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=1VPotzhpAGE">Paramore - Misery Business</a></li>
+          </ul>
+        <li>2006</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_msLrRDnvxVk6L-DH6OWH2-RWIu3x2sgPQ">Red Hot Chili Peppers - Stadium Arcadium [album]</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=TG5X4kOjEX8">The Killers - For Reasons Unknown</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=aviGAyS9TGs">AFI - Miss Murder</a></li>
+          </ul>
+        <li>2005</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_mHw0m4VSaOIEtJcIOna5VcPZYU9R-p_yU">Panic! At The Disco - A Fever You Can't Sweat Out</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=cjVQ36NhbMk">The Fray - How to Save a Life [song]</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=uhG-vLZrb-g">Fall Out Boy - Sugar, We're Goin' Down</a></li>
+          </ul>
+        <li>2004</li>
+          <ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Kill_Bill:_Volume_2">Kill Bill: Volume 2</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=ZAErD8xzjCM">The Killers - Smile Like You Mean It</a></li>
+          </ul>
+        <li>2003 - <a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_mznskEoj_ILsHxKsvj8Tb5JZpLJ6gOs0M">Jane's Addiction - Strays [album]</a></li>
+        <li>2002</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_mIzb8byci429atL6GqqYKvYw-GVHPR5jo">Red Hot Chili Peppers - By The Way [album]</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Courage_the_Cowardly_Dog">Courage the Cowardly Dog</a></li>
+          </ul>
+        <li>2001</li>
+          <ul>
+            <li>Music</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/watch?v=aqBKjRzmiRs">311 - Amber</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_npgPI6XUjvSBezvStiFxNTNrcNm8BnNuI">Puddle of Mudd - Come Clean</a></li>
+              </ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Courage_the_Cowardly_Dog">Courage the Cowardly Dog</a></li>
+          </ul>
+        <li>2000 - <a target="_blank" href="https://en.wikipedia.org/wiki/Courage_the_Cowardly_Dog">Courage the Cowardly Dog</a></li>
+        <li>1999</li>
+          <ul>
+            <li>Music</li>
+              <ul>
+                <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_kEbANGGtoT4n87DQ3ESZyh3l9LK5uWII0">Red Hot Chili Peppers - Californication [album]</a></li>
+                <li><a target="_blank" href="https://www.youtube.com/watch?v=fgT9zGkiLig">Incubus - Drive</a></li>
+              </ul>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Courage_the_Cowardly_Dog">Courage the Cowardly Dog</a></li>
+          </ul>
+        <li>1998</li>
+          <ul>
+            <li>Entertainment</li>
+              <ul>
+                <li><a target="_blank" href="https://en.wikipedia.org/wiki/Fear_and_Loathing_in_Las_Vegas_(film)">Fear and Loathing in Las Vegas</a></li>
+                <li><a target="_blank" href="https://en.wikipedia.org/wiki/Smoke_Signals_(film)">Smoke Signals</a></li>
+              </ul>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=O3dWBLoU--E">Hole - Celebrity Skin</a></li>
+          </ul>
+        <li>1996 - <a target="_blank" href="https://en.wikipedia.org/wiki/Beavis_and_Butt-Head_Do_America">Beavis and Butt-Head Do America</a></li>
+        <li>1995</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=PHzOOQfhPFg">No Doubt - Just A Girl</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=lYBIRHi5-o8">311 - Down</a></li>
+          </ul>
+        <li>1994</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/playlist?list=PLQXcAyvY0ao5IcNnYmokQ0cX-2C893Hyp">Nirvana - MTV Unplugged in New York</a></li>
+            <li><a target="_blank" href="https://en.wikipedia.org/wiki/Natural_Born_Killers">Natural Born Killers</a></li>
+          </ul>
+        <li>1991</li>
+          <ul>
+            <li><a target="_blank" href="https://www.youtube.com/playlist?list=OLAK5uy_nN3UV5AlNbRP9di-cmgC-ACgL8JLK6jGk">Nirvana - Nevermind</a></li>
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=lwlogyj7nFE">Red Hot Chili Peppers - Under the Bridge</a></li>
+          </ul>
+        <li>1987 - <a target="_blank" href="https://en.wikipedia.org/wiki/Raising_Arizona">Raising Arizona</a></li>
+        <li>1986 - <a target="_blank" href="https://en.wikipedia.org/wiki/True_Stories_(film)">True Stories</a></li>
+        <li>1965 - <a target="_blank" href="https://www.youtube.com/watch?v=yKI7c9x2lbM">The Yardbirds - For Your Love</a></li>
+      </ul>
+    </div>
+  </div>
+  <div id="footer">
+    <p><small>© 2021 Kire Uusimies</small></p>
+  </div>
+</body>
+</html>
